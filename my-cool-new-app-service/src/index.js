3f5f20e65e65f75e089e116e24449953c5a25344
Wagner Paulino <wagnersantos9778@gmail.com>
2020-01-25 18:23:16 -0300

    Ajustes finais

--- my-cool-new-app-service/src/index.js
+++ my-cool-new-app-service/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors')
 
 const app = express();
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
 
 require('./controllers/lista-desejo')(app);
 
