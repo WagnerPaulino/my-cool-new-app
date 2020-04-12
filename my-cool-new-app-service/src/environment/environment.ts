695b346c84b676fe14ad27ae8f811ae9b288e951
Wagner Paulino <wagnersantos9778@gmail.com>
2020-04-12 18:06:38 -0300

    instalação do firebase

--- my-cool-new-app-service/src/environment/environment.ts
+++ my-cool-new-app-service/src/environment/environment.ts
@@ -1,3 +1,16 @@
 export function getHostDataBase(): string {
     return `db:27017`;
 }
+
+export function getFirebaseConfig() {
+    return {
+        apiKey: "AIzaSyBqoIiOiiEgJ8iumz71H3fTLE05qW7IaJQ",
+        authDomain: "my-cool-new-app-94ac1.firebaseapp.com",
+        databaseURL: "https://my-cool-new-app-94ac1.firebaseio.com",
+        projectId: "my-cool-new-app-94ac1",
+        storageBucket: "my-cool-new-app-94ac1.appspot.com",
+        messagingSenderId: "466887974288",
+        appId: "1:466887974288:web:3c5b2f278313acadda6715",
+        measurementId: "G-8CL21547L3"
+    };
+}
