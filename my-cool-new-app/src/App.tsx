a73408a07526d8d770537e247793827faa8aa509
Wagner Paulino <wagnersantos9778@gmail.com>
2020-04-22 22:07:30 -0300

    Ajuste na arquitetura

--- my-cool-new-app/src/App.tsx
+++ my-cool-new-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector, Provider } from 'react-redux';
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { getCurrentUser } from './actions/usuario-actions';
 import './App.css';
@@ -7,8 +7,21 @@ import PrivateRoute from './commons/private-router';
 import { ListaDesejosEditComponent } from './pages/lista-desejos-edit-component';
 import { ListaDesejosListComponent } from './pages/lista-desejos-list-component';
 import { LoginComponent } from './pages/login.component';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { listaDesejosList, listaDesejosEdit } from './reducers/desejos-reducers';
+import { usuarioReducer } from './reducers/usuarios-reducers';
+import customMiddleware from './commons/custom-middleware';
+import Firebase, { FirebaseContext } from './environment/context';
 
-const App = () => {
+const rootReducer = combineReducers({
+  listaDesejos: listaDesejosList,
+  desejo: listaDesejosEdit,
+  usuario: usuarioReducer
+})
+
+const store = createStore(rootReducer, applyMiddleware(customMiddleware))
+
+const RouterDefinitions = () => {
 
   const user = useSelector((store: any) => store.usuario)
   const dispatch = useDispatch();
@@ -29,6 +42,16 @@ const App = () => {
         </Switch>
       </div>
     </Router >
+  )
+}
+
+const App = () => {
+  return (
+    <FirebaseContext.Provider value={new Firebase()}>
+      <Provider store={store}>
+        <RouterDefinitions />
+      </Provider>
+    </FirebaseContext.Provider>
   )
 }
 
--- my-cool-new-app/src/index.tsx
+++ my-cool-new-app/src/index.tsx
@@ -1,31 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Firebase, { FirebaseContext } from './environment/context';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { combineReducers, createStore, applyMiddleware } from 'redux';
-import { listaDesejosList, listaDesejosEdit } from './reducers/desejos-reducers';
-import { usuarioReducer } from './reducers/usuarios-reducers';
-import customMiddleware from './commons/custom-middleware';
-import { Provider } from 'react-redux';
-
-const rootReducer = combineReducers({
-    listaDesejos: listaDesejosList,
-    desejo: listaDesejosEdit,
-    usuario: usuarioReducer
-})
-
-const store = createStore(rootReducer, applyMiddleware(customMiddleware))
 
 ReactDOM.render(
-    <FirebaseContext.Provider value={new Firebase()}>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </FirebaseContext.Provider>,
+    <App />,
     document.getElementById('root'));
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
