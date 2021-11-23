22729d0e491526acaf0b6fdde8cf0b9e92f50836
Wagner Paulino <wagnersantos9778@gmail.com>
2021-11-22 23:00:21 -0300

    refaração

--- my-cool-new-app/src/actions/desejos-actions.tsx
+++ my-cool-new-app/src/actions/desejos-actions.tsx
@@ -1,32 +1,28 @@
-import { getHostBackend } from "../environment/environment";
+import { getAxios, getHostBackend } from "../environment/environment";
 import { ListaDesejos } from '../models/ListaDesejos';
 import { LISTA_DESEJOS_DELETE, LISTA_DESEJOS_EDIT, LISTA_DESEJOS_LIST, LISTA_DESEJOS_LOAD, LISTA_DESEJOS_LIST_SEARCH } from "./desejos-types";
 import Firebase from "../environment/context";
 const firebase = new Firebase()
 export function findAll(): (store: any) => void {
     return (store: any) => {
-        fetch(`http://${getHostBackend()}/api/lista-desejos/findAll`,
-            { method: 'POST', body: JSON.stringify({ nome: firebase.getCurrentAuth()?.currentUser?.displayName }), headers: { "Content-Type": "application/json" } })
-            .then(response => response.json().then(value => store.dispatch(
-                {
-                    listaDesejos: value,
-                    type: LISTA_DESEJOS_LIST
-                }
-            )));
+        getAxios().post('/lista-desejos/findAll', { nome: firebase.getCurrentAuth()?.currentUser?.displayName }).then(value => store.dispatch(
+            {
+                listaDesejos: value.data,
+                type: LISTA_DESEJOS_LIST
+            }
+        ));
     }
 }
 
 export function findByName(nome: string): (store: any) => void {
     let body = { nome: nome }
     return (store: any) => {
-        fetch(`http://${getHostBackend()}/api/lista-desejos/find-by-nome`,
-            { method: 'POST', body: JSON.stringify(body), headers: { "Content-Type": "application/json" } })
-            .then(response => response.json().then(value => store.dispatch(
-                {
-                    listaDesejos: value,
-                    type: LISTA_DESEJOS_LIST_SEARCH
-                }
-            )));
+        getAxios().post('/lista-desejos/find-by-nome', body).then(value => store.dispatch(
+            {
+                listaDesejos: value.data,
+                type: LISTA_DESEJOS_LIST_SEARCH
+            }
+        ));
     }
 }
 
@@ -44,32 +40,34 @@ export function findOne(key: string): (store: any) => void {
 export function save(desejo: ListaDesejos, history: any): (store: any) => void {
     const obj = { desejo: desejo, usuario: { nome: firebase.getCurrentAuth()?.currentUser?.displayName } }
     return (store: any) => {
-        fetch(`http://${getHostBackend()}/api/lista-desejos/`, { method: 'POST', body: JSON.stringify(obj), headers: { "Content-Type": "application/json" } }).then(response => response.json().then(value => {
-            store.dispatch({
-                type: LISTA_DESEJOS_EDIT,
-                listaDesejo: value
-            })
-            history('/')
-        }))
+        getAxios().post('lista-desejos', obj).then(value => {
+            history('/');
+            store.dispatch(
+                {
+                    listaDesejos: value.data,
+                    type: LISTA_DESEJOS_EDIT
+                }
+            );
+        });
     }
 }
 
 export function excluir(desejo: ListaDesejos, history: any): (store: any) => void {
     return (store: any) => {
-        fetch(`http://${getHostBackend()}/api/lista-desejos/${desejo._id}`, { method: 'DELETE' }).then(() => {
-            history('/')
-            store.dispatch({
-                type: LISTA_DESEJOS_DELETE,
-                listaDesejo: new ListaDesejos()
-            })
-        })
+        getAxios().delete(`/lista-desejos/${desejo._id}`).then(value => {
+            history('/');
+            store.dispatch(
+                {
+                    type: LISTA_DESEJOS_DELETE,
+                    listaDesejo: new ListaDesejos()
+                }
+            );
+        });
     }
 }
 
 export async function exist(nome: string): Promise<boolean> {
     let body = { nome: nome }
-    const response = await fetch(`http://${getHostBackend()}/api/lista-desejos/find-by-nome`,
-        { method: 'POST', body: JSON.stringify(body), headers: { "Content-Type": "application/json" } });
-    const desejos: ListaDesejos[] = await response.json();
+    const desejos: ListaDesejos[] = (await getAxios().post("/lista-desejos/find-by-nome", body)).data;
     return desejos.length > 0;
 }
--- my-cool-new-app/src/environment/environment.ts
+++ my-cool-new-app/src/environment/environment.ts
@@ -1,5 +1,13 @@
+import axios, { AxiosInstance } from "axios";
+
 export function getHostBackend(): string {
     return `localhost:3000`;
+}
+
+export function getAxios(): AxiosInstance {
+    return axios.create({
+        baseURL: `http://${getHostBackend()}/api`
+    });
 }
 
 export function getFirebaseConfig() {
